84ae392740b8155be242766a50c102d5e4cd41b8
qbert0 <126874637+qbert0@users.noreply.github.com>
2026-03-24 12:03:22 +0700

    change use nuxt js

---
+++ Client_service/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-blue-600  font-xs"> hê2llo</h1>
+  </div>
+</template>
---
+++ Client_service/nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'], 
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
